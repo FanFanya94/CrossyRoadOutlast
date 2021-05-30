fd133e044e2fa947ba703d12e63be0720d9570d3
COMP2\Igor <IDyachenko2008@yandex.ru>
2021-05-30 09:40:13 +0200

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -122,7 +122,6 @@ public class PlayerController : MonoBehaviour
 
     private void EasedMove() // Smoothly moves player to a new position 
     {
-        //transform.position = Vector3.Lerp(transform.position, targetPos, movingSpeed);
         transform.position = Vector3.Lerp(transform.position, targetPos, movingSpeed * Time.deltaTime);
     }
 
